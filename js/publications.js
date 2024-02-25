1bbf55e28eb8d7d41b63b268b3f8cfc5338e474c
Alexis Cooper <peco6819@colorado.edu>
2024-02-24 21:52:15 -0700

    Updated the look and redid Publications page (WIP). Also rewrote the includes (header, navbar, footer).

--- js/publications.js
+++ js/publications.js
@@ -1,10 +1,25 @@
-const fs = require('fs')
+window.addEventListener('load', function() {
+    getPubs();
+})
 
-function decodeCSV(csv){
-    let rows = csv.split(splitter="\n")
+function getPubs()
+{
+    var filepath = '../resources/pubs.csv'
 
-    let pubs = []
-    let row_split = []
+    var csv = fetch(filepath)
+    .then(response => response.text())
+        .then(data => {
+            var pubs = decodePubs(data);
+            addPubs(pubs);
+        })
+}
+
+function decodePubs(csv)
+{
+    var rows = csv.split(splitter="\n")
+
+    var pubs = []
+    var row_split = []
 
     rows.forEach(element =>
     {
@@ -23,9 +38,85 @@ function decodeCSV(csv){
     return pubs
 }
 
-var filepath = 'resources/pubs.csv'
-var csv = fs.readFile(filepath, 'utf8', (err, data) => {
-    if(err) throw err;
-    data = decodeCSV(data);
-    console.log("hiiii")
-})
+async function addPubs(pubs)
+{
+    const groupByYear = (pubs) => pubs.reduce((acc, pub) => {
+        let annum = pub.year.trim()
+        if(!acc[annum]) {
+            acc[annum] = [];
+        }
+        
+        acc[annum].push(pub);
+
+        return acc;
+    }, {})
+
+    var pubs_grouped = groupByYear(pubs);
+    console.log(pubs_grouped);
+
+    var i = 0;
+
+    Object.entries(pubs_grouped).reverse().forEach(([year, publications]) => {
+        var this_year = document.createElement("div");
+        this_year.setAttribute("class", "pub_year");
+        this_year.setAttribute("id", year);
+        
+        year_label = document.createElement("div");
+        year_label.setAttribute("class", "year_label");
+        year_label.innerHTML = year;
+        this_year.appendChild(year_label);
+
+        publications.forEach(pub => {
+            var pub_span = document.createElement("div");
+            pub_span.setAttribute("class", "pub_span")
+            
+            pub_title = document.createElement("div");
+            pub_title.setAttribute("class", "pub_title")
+            pub_title.innerHTML = pub.title;
+
+            pub_span.appendChild(pub_title)
+
+            pub_authors = document.createElement("div");
+            pub_authors.setAttribute("class", "pub_authors")
+            pub_authors.innerHTML = pub.authors;
+
+            pub_span.appendChild(pub_authors)
+
+            pub_journal = pub.journal;
+
+            pub_journal = document.createElement("div");
+            pub_journal.setAttribute("class", "pub_journal")
+            pub_journal.innerHTML = pub.journal;
+
+            if(pub.pages != "")
+            {
+                pub_journal.innerHTML += " (pp. " + pub.pages + ")"
+            }
+
+            pub_span.appendChild(pub_journal)
+
+            pub_link = document.createElement("div");
+            pub_link.setAttribute("class", "pub_link")
+            
+            if(pub.link.trim().length !== 0)
+            {
+                let link = document.createElement("a");
+                link.setAttribute("href", pub.link);
+                link.innerHTML = "[link]";
+                pub_link.setAttribute("style", "padding-bottom:10px;")
+
+                pub_link.appendChild(link);
+            }
+            else
+            {
+                pub_link.innerHTML = "<br>"
+            }
+
+            pub_span.appendChild(pub_link);
+
+            this_year.appendChild(pub_span);
+        })
+        
+        document.getElementById("main_body").appendChild(this_year);
+    })
+}
