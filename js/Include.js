05597587891ea10b41ec8551dba30892fd015dca
Alexis Cooper <peco6819@colorado.edu>
2024-02-24 21:23:26 -0700

    Cleaned up includes and redid publications page (still WIP but functions)

---
+++ js/Include.js
@@ -0,0 +1,50 @@
+async function createSharedElements() {
+    await createNavbar();
+    await createHeader();
+    await createFooter();
+}
+
+async function createNavbar() {
+    fetch('../include/navigation.html')
+        .then(response => response.text())
+            .then(data => {
+                var navbar = document.createElement('div');
+
+                navbar.setAttribute("id", "navbar")
+                navbar.innerHTML = data;
+
+                document.getElementById('wrapper').prepend(navbar);
+            })
+
+    return Promise.resolve();
+}
+
+async function createHeader() {
+    fetch('../include/title.html')
+        .then(response => response.text())
+            .then(data => {
+                var title = document.createElement('div');
+
+                title.setAttribute("id", "header")
+                title.innerHTML = data;
+
+                document.getElementById('wrapper').prepend(title);
+            })
+
+    return Promise.resolve();
+}
+
+async function createFooter() {
+    fetch('../include/footer.html')
+        .then(response => response.text())
+            .then(data => {
+                var navbar = document.createElement('footer');
+
+                navbar.setAttribute("id", "footer")
+                navbar.innerHTML = data;
+
+                document.getElementById('wrapper').append(navbar);
+            })
+    
+    return Promise.resolve();
+}
---
+++ server.js
@@ -0,0 +1,18 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+// const path = require('path');
+
+// // Middleware
+// app.use(express.json());
+
+// // Define a route
+// app.get('/', (req, res) => {
+//   console.log(__dirname)
+//   res.sendFile(path.join(__dirname, '/src/index.html'));
+// });
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`EListening at http://localhost:${port}`);
+// });
